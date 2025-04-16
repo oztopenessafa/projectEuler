e5218edb280dcd2b4fa02c9ec2df24ccde9a79d9
oztopenessafa <oztopenessafa@gmail.com>
2025-04-16 22:13:32 +0300

    Yorum satırları eklendi.

--- problem1.py
+++ problem1.py
@@ -1,5 +1,5 @@
 sum = 0
 for num in range(1000):
-    if num % 3 == 0 or num % 5 == 0:
+    if num % 3 == 0 or num % 5 == 0: #Add the numbers that can be divided to 3 either 5 to sum
        sum += num
 print(sum)
--- problem2.py
+++ problem2.py
@@ -1,10 +1,17 @@
+#Initial starting points of the fibonacci sequence
 num1 = 1
 num2 = 2
+
 sum = 0
+
+#num1 must be the limiter as it's bound to be smaller than num2
 while num1 < 4000000:
+    #Only num1 is added in order to prevent sum of duplicate numbers
     if num1 % 2 == 0:
        sum += num1
+    #Store num2 as temp in order for num1 to assign as the former num2 after num2 is assigned as num1+num2
     temp = num2
     num2 += num1
     num1 = temp
+
 print(sum)
